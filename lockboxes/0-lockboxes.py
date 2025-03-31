c0296f247f1853b771857e970f9a6d58c06457c0
LOICKITOKO <loicassaelkm@gmail.com>
2025-03-31 15:48:06 +0200

    review task

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -3,20 +3,22 @@
 Determines if all boxes can be unlocked.
 """
 
+
 def canUnlockAll(boxes):
     """
     Checks if all the boxes can be unlocked.
+
     :param boxes: List of lists, where each list contains keys to other boxes.
     :return: True if all boxes can be opened, else False.
     """
     n = len(boxes)
-    opened = set([0])  # Start with the first box unlocked
+    opened = {0}  # Start with the first box unlocked
     keys = set(boxes[0])  # Keys found in the first box
-    
+
     while keys:
         key = keys.pop()  # Take a key from the set
         if key < n and key not in opened:
             opened.add(key)  # Unlock the box
             keys.update(boxes[key])  # Add new keys from the opened box
-    
+
     return len(opened) == n
